86b7ea620ac002c24c5d880c6850111c5564d759
soumyamb98 <soumyamb98@outlook.com>
2021-12-04 12:13:14 +0530

    m

--- angular9/src/app/updateauthors/updateauthors.component.ts
+++ angular9/src/app/updateauthors/updateauthors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthorservicaService } from '../authorservica.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-updateauthors',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./updateauthors.component.css']
 })
 export class UpdateauthorsComponent implements OnInit {
-
-  constructor() { }
+  authors= {
+    authorsId:'',
+    title: '',
+        author:'',
+        genre: '',
+        // image: [''],
+        // imageUrl: ['']
+    imageUrl: ''
+  
+  }
+  constructor(private router:Router, private authorsservice:AuthorservicaService) { }
 
   ngOnInit(): void {
+    let authorsId = localStorage.getItem("editauthorsId");
+    this.authorsservice.getauthor(authorsId).subscribe((data)=>{
+      this.authors=JSON.parse(JSON.stringify(data));
+  })
   }
-
+  updateauthors()
+  {    
+    this.authorsservice.updateauthors(this.authors);   
+    alert("Success");
+    this.router.navigate(['authors']);
+  }
 }
